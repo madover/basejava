252f065a2375ff344d6b7bfcad62b16e410232d6
madover <evgeny86@gmail.com>
2020-03-13 22:59:22 +0500

    HW1 fixed

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,38 +3,37 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
-        for (int i = 0; i < this.size(); i++) {
-            this.storage[i] = null;
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        this.storage[this.size()] = r;
+        storage[size()] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        try {
-            for (int i = 0; i < this.size(); i++) {
-                if (this.storage[i].toString().equals(uuid)) {
-                    return this.storage[i];
+            for (int i = 0; i < size(); i++) {
+                if (storage[i].toString().equals(uuid)) {
+                    return storage[i];
                 }
             }
-        } catch (NullPointerException npe) {
-            System.out.println("UUID not found!");
-        }
-
         return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < this.size(); i++) {
-            if (this.storage[i].toString().equals(uuid)) {
-                for (int j = i; j < this.size() - 1; j++) {
-                    this.storage[j] = this.storage[j + 1];
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].toString().equals(uuid)) {
+                for (int j = i; j < size() - 1; j++) {
+                    storage[j] = storage[j + 1];
                 }
-                this.storage[this.size() - 1] = null;
+                storage[size() - 1] = null;
+                size--;
                 break;
             }
         }
@@ -44,16 +43,12 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] allCV = new Resume[this.size()];
-        System.arraycopy(this.storage, 0, allCV, 0, this.size());
+        Resume[] allCV = new Resume[size()];
+        System.arraycopy(storage, 0, allCV, 0, size());
         return allCV;
     }
 
     int size() {
-        int size = 0;
-        while (this.storage[size] != null) {
-            size++;
-        }
         return size;
     }
 }
